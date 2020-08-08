c09cf8c176f036d3d19bd06f1061c1e053b34adf
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-08-08 11:58:13 -0300

    host caller example

---
+++ examples/host-caller/README.md
@@ -0,0 +1,11 @@
+# GO host caller example
+
+- Compile project `go build main.go`
+- run with arguments `./main https://www.google.com https://golang.org https://golang.org/doc/`
+```
+caller started...
+0.42s - https://www.google.com
+0.48s - https://golang.org/doc/
+0.90s - https://golang.org
+
+```
---
+++ examples/host-caller/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Println("caller started...")
+	callerChan := make(chan string)
+	hosts := os.Args[1:]
+
+	for _, host := range hosts {
+		go measure(host, callerChan)
+	}
+
+	for range hosts {
+		fmt.Println(<-callerChan)
+	}
+}
+
+func measure(host string, callerChan chan string) {
+	startTime := time.Now()
+	_, err := call(host)
+
+	if err != nil {
+		callerChan <- "failed to call " + host
+		return
+	}
+
+	duration := time.Since(startTime).Seconds()
+	callerChan <- fmt.Sprintf("%.2fs - %s", duration, host)
+}
+
+func call(host string) ([]byte, error) {
+	response, err := http.Get(host)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
